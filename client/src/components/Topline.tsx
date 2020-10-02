dc8adf28f95d41aba3027a07118b502b3af0f707
Björn Ruberg <bjoern@ruberg-lammok.de>
2020-10-02 11:20:31 +0200

    Translate banner

--- client/src/components/Topline.tsx
+++ client/src/components/Topline.tsx
@@ -1,9 +1,12 @@
+import { TranslateContext } from "@denysvuika/preact-translate";
 import { FunctionalComponent, h } from "preact";
+import { useContext } from "preact/hooks";
 
 const Topline: FunctionalComponent = () => {
+  const { t } = useContext(TranslateContext);
   return (
     <div class="bg-gray-800 p-1 px-2 text-white text-xs">
-      <strong>Pricora</strong> - verschlüsselt, kontaktlos, OpenSource.
+      <strong>Pricora</strong> - {t("navigation.banner")}.
     </div>
   );
 };
