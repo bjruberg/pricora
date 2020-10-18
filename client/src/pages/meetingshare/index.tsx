b84fcd75f34d2a1d579b43916d9331cd8ef46596
Björn Ruberg <bjoern@ruberg-lammok.de>
2020-10-18 13:14:46 +0200

    Add fullscreen button to share page

--- client/src/pages/meetingshare/index.tsx
+++ client/src/pages/meetingshare/index.tsx
@@ -1,12 +1,13 @@
 import { FunctionalComponent, h } from "preact";
 import { TranslateContext } from "@denysvuika/preact-translate";
-import { useContext } from "preact/hooks";
+import { useCallback, useContext } from "preact/hooks";
 import { useQuery } from "@urql/preact";
 
 import gql from "graphql-tag";
 import QRCode from "qrcode.react";
 
 import PageContainer from "../../components/PageContainer";
+import Button from "../../ui/button";
 import { useShareLink } from "../../utils/useShareLink";
 import useInterval from "../../utils/useInterval";
 import { GetMeetingShareQuery, GetMeetingShareQueryVariables } from "./index.gql";
@@ -30,6 +31,15 @@ const MeetingSharePage: FunctionalComponent<MeetingShareProps> = ({ uuid }) => {
   const { t } = useContext(TranslateContext);
 
   const [, generateToken, shareLink] = useShareLink(uuid);
+  const toggleFullScreen = useCallback(() => {
+    if (!document.fullscreenElement) {
+      void document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        void document.exitFullscreen();
+      }
+    }
+  }, []);
 
   useInterval(() => {
     void generateToken({ meetingId: uuid });
@@ -43,9 +53,16 @@ const MeetingSharePage: FunctionalComponent<MeetingShareProps> = ({ uuid }) => {
 
   return data ? (
     <PageContainer>
-      <h1 className="mb-2">
-        {t("pages.meetingshare.title")}: <strong>{data.meeting.title}</strong>
-      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 mb-2">
+        <h1 className="mb-2">
+          {t("pages.meetingshare.title")}: <strong>{data.meeting.title}</strong>
+        </h1>
+        <div className="md:justify-self-end">
+          <Button onClick={toggleFullScreen} slim variant="secondary">
+            {t("pages.meetingshare.toggleFullScreen")}
+          </Button>
+        </div>
+      </div>
       {shareLink ? (
         <div className="mt-4 flex flex-col items-center">
           <QRCode className="m-2 mb-4" size={300} value={shareLink} />
